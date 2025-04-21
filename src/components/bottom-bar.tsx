697c4c4fe4a93a44d246c35558ab2f19007c729c
TonyRodIv <antony.rodriguesiv@gmail.com>
2025-04-21 20:27:10 -0300

    feat: initialize React project with Vite, TypeScript, and ESLint
    - Added package.json with dependencies for React, Vite, and TypeScript.
    - Included SVG assets for Vite and React logos.
    - Created BottomBar component with basic structure.
    - Added global styles in index.css for layout and design.
    - Set up main entry point in main.tsx to render BottomBar.
    - Configured TypeScript with app and node configurations.
    - Created vite.config.ts for Vite setup with React plugin.

---
+++ src/components/bottom-bar.tsx
@@ -0,0 +1,13 @@
+export default function bottomBar() {
+    return(
+        <main className="bottomBar">
+           <article className="logoTop"></article>
+
+           <section>
+            <article className="dPad"></article>
+            <button className="btnAB btnA">A</button>
+            <button className="btnAB btnB">B</button>
+           </section>
+        </main>
+    )
+}
